b0c3b4aa08af3fa2d0c65645def7375a671ad7f5
Mangesh Sakordekar <mangeshsakordekar@gmail.com>
2025-09-02 19:20:17 +0530

    Initial Commit - Working Floating Mute button App

---
+++ app/src/main/java/com/example/floatingmute/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.floatingmute
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+
+        startButton.setOnClickListener {
+            if (Settings.canDrawOverlays(this)) {
+                startService(Intent(this, FloatingButtonService::class.java))
+            } else {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivity(intent)
+            }
+        }
+
+        stopButton.setOnClickListener {
+            stopService(Intent(this, FloatingButtonService::class.java))
+        }
+    }
+}
